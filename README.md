652e258c1c5a992326105f7c5eeae8c4fbc11178
Clark19 <clarkgable1@gmail.com>
2022-12-06 23:44:57 +0900

    Feature: 노드, 리액트 기초 강의 초기 구현. 몽고디비까지 초기 연결. 초기 환경 변수 추가

---
+++ README.md
@@ -0,0 +1,9 @@
+# 초기 설정
+
+npm init
+git init
+npm i express mongoose body-parser --save
+npm i nodemon --save-dev
+(package.json 안에 "scripts": {
+"start": "node index.js",
+"dev": "nodemon index.js", ...} 넣기)
---
+++ index.js
@@ -0,0 +1,37 @@
+const exprerss = require("express");
+const app = exprerss();
+const port = 5000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  })
+  .then(() => console.log("몽고디비 연결"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("hello"));
+
+app.post("/register", (req, res) => {
+  // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣음.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
+app.listen(port, () => console.log("ap start " + port));
