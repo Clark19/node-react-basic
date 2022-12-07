edbcfc3c442298d5754e345ea95775edcd29df85
Clark19 <clarkgable1@gmail.com>
2022-12-08 01:27:40 +0900

    Feature: 로그아웃( jwt 토큰을 RDB 에서 삭제) 구현

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const { auth } = require("./middleware/auth");
+
 const { User } = require("./models/User");
 
 // application/x-www-form-urlencoded
@@ -26,7 +28,7 @@ mongoose
 
 app.get("/", (req, res) => res.send("hello"));
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원 가입 할때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터베이스에 넣음.
   const user = new User(req.body);
@@ -36,7 +38,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
       return res.json({
@@ -59,6 +61,27 @@ app.post("/login", (req, res) => {
           .json({ loginSuccess: true, userId: user._id });
       });
     });
+  });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기까지 호출이 된다는 것은 auth 미들웨어를 에러 없이 통과(next())했다는 것이다.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.usr.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({ success: true });
   });
 });
 
