9cba52cb4353a1358393cb7bcda79e8d17d7e3d5
Clark19 <clarkgable1@gmail.com>
2022-12-10 20:40:19 +0900

    Feature: 회원가입 구현 - Redux 사용

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_actions/user_action";
 
-function RegisterPage() {
-  return <div>RegisterPage</div>;
+function RegisterPage(props) {
+  const dispatch = useDispatch();
+  const [Email, setEmail] = useState("");
+  const [Name, setName] = useState("");
+  const [Password, setPassword] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value);
+  };
+
+  const onPasswordHandler = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const onConfirmPasswordHandler = (e) => {
+    setConfirmPassword(e.currentTarget.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if (Password !== ConfirmPassword) {
+      return alert("비밀번호와 비밀번호 확인은 같아야 합니다.");
+    }
+
+    const body = {
+      email: Email,
+      name: Name,
+      password: Password,
+    };
+
+    // Redux 안쓸땐 여기에서 서버에 요청 보냄
+    // ex) axion.post("/api/users/register", body)
+
+    dispatch(registerUser(body)).then((res) => {
+      if (res.payload.success) {
+        // props.history.push("/");
+        navigate("/login");
+      } else {
+        alert("회원가입 Error");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <h2>회원가입</h2>
+        <label> Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>이름</label>
+        <input type="text" value={Name} onChange={onNameHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
+        <br />
+        <button type="submit">회원가입</button>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;
