ab794744a04dcf596067ace5a5ac7e838633413b
Clark19 <clarkgable1@gmail.com>
2022-12-11 16:52:53 +0900

    Feature: 로그아웃 구현

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # 초기 설정
 
+```
 npm init
 git init
 npm i express mongoose body-parser --save
@@ -10,3 +11,9 @@ npm i bcrypt jsonwebtoken cookie-parser --save
 "dev": "nodemon index.js", ...} 넣기)
 
 npm i concurrently --save
+(package.json 파일 안 "scripts": {...} 안에 아래 넣기
+"both": "concurrently \"npm run dev\" \"npm run start --prefix client\"" )
+```
+
+- 강의 원본 레포 : https://github.com/jaewonhimnae/boiler-plate-ko
+- 원본인강 : https://youtube.com/playlist?list=PL9a7QRYt5fqkZC9jc7jntD1WuAogjo_9T
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,27 @@
 import React, { useEffect } from "react";
 
 import axios from "axios";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     axios
       // .get("http://localhost:5000/api/hello")
       .get("/api/hello")
       .then((res) => console.log(res));
   }, []);
+
+  const onClickHandler = async () => {
+    const rst = await axios.get("/api/users/logout");
+    console.log("로그아웃:", rst.data);
+    if (rst.data.success) {
+      navigate("/login");
+    } else {
+      alert("로그아웃 실패");
+    }
+  };
 
   return (
     <div
@@ -21,6 +34,8 @@ function LandingPage() {
       }}
     >
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
